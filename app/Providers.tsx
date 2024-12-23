36a9109c5c40a71e2be3822f1a628074b90a0e3f
Sroysawan Kladcheydee <sroysawan.kcd@gmail.com>
2024-12-23 13:17:37 +0700

    feat: Add Providers component for theme and toaster integration

---
+++ app/Providers.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { ThemeProvider } from './theme-provider'
+import { Toaster } from "@/components/ui/toaster"
+const Providers = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <>
+            <ThemeProvider
+                attribute="class"
+                defaultTheme="system"
+                enableSystem
+                disableTransitionOnChange
+            >
+                {children}
+                <Toaster />
+            </ThemeProvider>
+        </>
+    )
+}
+
+export default Providers
