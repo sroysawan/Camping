54167a112bff40fe434c87d137abeea2c8aebe13
Sroysawan Kladcheydee <sroysawan.kcd@gmail.com>
2024-12-24 21:17:02 +0700

    feat: Add ProvinceInput component for selecting provinces

---
+++ components/form/ProvinceInput.tsx
@@ -0,0 +1,41 @@
+import { Label } from '@/components/ui/label'
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select"
+import { provinces } from '@/utils/provinces'
+
+const ProvinceInput = ({ defaultValue }: { defaultValue?: string }) => {
+    const name = 'province'
+    return (
+        <div className='mb-2'>
+            <Label htmlFor={name} className='capitalize'>{name}</Label>
+            <Select
+                defaultValue={defaultValue || provinces[0].PROVINCE_NAME}
+                name={name}
+                required
+            >
+                <SelectTrigger>
+                    <SelectValue />
+                </SelectTrigger>
+                <SelectContent>
+                    {provinces.map((item) => {
+                        return (
+                            <SelectItem value={item.PROVINCE_NAME} key={item.PROVINCE_ID}>
+                                <span className='capitalize flex items-center gap-2'>
+                                    {item.PROVINCE_NAME}
+                                </span>
+                            </SelectItem>
+                        )
+                    }
+                    )}
+                </SelectContent>
+            </Select>
+        </div>
+    )
+}
+
+export default ProvinceInput
