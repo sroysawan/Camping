dca0d9788c7c534d4029b8638c76548c89f489f4
Sroysawan Kladcheydee <sroysawan.kcd@gmail.com>
2024-12-25 22:01:17 +0700

    feat: Add CreateLandmark page with form and map integration

---
+++ app/camp/create/page.tsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import { SubmitButton } from '@/components/form/Buttons'
+import FormContainer from '@/components/form/FormContainer'
+import FormInput from '@/components/form/FormInput'
+import { createLandmarkAction } from '@/actions/action'
+import { currentUser } from '@clerk/nextjs/server'
+import { redirect } from 'next/navigation'
+import CategoryInput from '@/components/form/CategoryInput'
+import TextAreaInput from '@/components/form/TextAreaInput'
+import ProvinceInput from '@/components/form/ProvinceInput'
+import MapLandmark from '@/components/map/MapLandmark'
+import ImageInput from '@/components/form/ImageInput'
+
+const CreateLandmark = async () => {
+
+    return (
+        <section>
+            <h1 className='text-2xl font-semibold mb-8 capitalize'>Create Landmark</h1>
+            <div className='border p-8 rounded-md'>
+                <FormContainer action={createLandmarkAction}>
+                    <div className='grid md:grid-cols-2 gap-4 mt-4'>
+                        <FormInput
+                            name="name"
+                            label="Land Name"
+                            type="text"
+                            placeholder="Land Name"
+                        />
+                        <CategoryInput />
+                    </div>
+                    <TextAreaInput name="description"/>
+                    <div className='grid md:grid-cols-2 gap-4 mt-4'>
+                        <FormInput
+                            name="price"
+                            label="Price"
+                            type="number"
+                            placeholder="Price"
+                        />
+                        <ProvinceInput />
+                    </div>
+                    <ImageInput/>
+                    <MapLandmark />
+                    <SubmitButton size='lg' text="Create Landmark" />
+                </FormContainer>
+            </div>
+        </section>
+    )
+}
+
+export default CreateLandmark
