20b68d48c4632ec70933bd6803bf906aa92705f3
Sroysawan Kladcheydee <sroysawan.kcd@gmail.com>
2024-12-25 12:25:35 +0700

    feat: Implement DropDownListMenu component with authentication support

---
+++ components/Navbar/DropDownListMenu.tsx
@@ -0,0 +1,73 @@
+import { AlignLeft } from "lucide-react"
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { Button } from "../ui/button"
+import UserIcon from "./UserIcon"
+import Link from "next/link"
+import { links } from "@/utils/links"
+
+import SignOutLinks from "./SignOutLinks"
+import { SignedIn, SignedOut, SignInButton, SignUpButton } from '@clerk/nextjs'
+
+const DropDownListMenu = () => {
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <Button variant='outline'>
+                    <AlignLeft />
+                    <UserIcon />
+                </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+                <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+
+                {/* ล็อคเอ้าแล้ว */}
+                <SignedOut>
+                    <DropdownMenuItem >
+                        <SignInButton mode="modal">
+                            <button>
+                                Log in
+                            </button>
+                        </SignInButton>
+                    </DropdownMenuItem>
+
+                    <DropdownMenuItem >
+                        <SignUpButton mode="modal">
+                            <button>
+                                Sign Up
+                            </button>
+                        </SignUpButton>
+                    </DropdownMenuItem>
+                </SignedOut>
+
+                {/* ล็อคอินแล้ว */}
+                <SignedIn>
+                    {
+                        links.map((item, index) => {
+                            return (
+                                <DropdownMenuItem key={index}>
+                                    <Link href={item.href}>
+                                        {item.label}
+                                    </Link>
+                                </DropdownMenuItem>
+                            )
+                        })
+                    }
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem >
+                        <SignOutLinks />
+                    </DropdownMenuItem>
+                </SignedIn>
+            </DropdownMenuContent>
+        </DropdownMenu>
+    )
+}
+
+export default DropDownListMenu
