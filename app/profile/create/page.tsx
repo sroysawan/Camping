fd3dca6fd8df9b6a8d2a63586ddc97a798e163aa
Sroysawan Kladcheydee <sroysawan.kcd@gmail.com>
2024-12-25 22:02:05 +0700

    feat: Add CreateProfile page for new user profile creation

---
+++ app/profile/create/page.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { SubmitButton } from '@/components/form/Buttons'
+import FormContainer from '@/components/form/FormContainer'
+import FormInput from '@/components/form/FormInput'
+import { createProfileAction } from '@/actions/action'
+import { currentUser } from '@clerk/nextjs/server'
+import { redirect } from 'next/navigation'
+
+const CreateProfile = async () => {
+    const user = await currentUser()
+    if (user?.privateMetadata.hasProfile) redirect('/')
+        
+    return (
+        <section>
+            <h1 className='text-2xl font-semibold mb-8 capitalize'>New User</h1>
+            <div className='border p-8 rounded-md'>
+                <FormContainer action={createProfileAction}>
+                    <div className='grid md:grid-cols-2 gap-4 mt-4'>
+                        <FormInput
+                            name="firstName"
+                            label="First Name"
+                            type="text"
+                            placeholder="First Name"
+                        />
+                        <FormInput
+                            name="lastName"
+                            label='Last Name'
+                            type='text'
+                            placeholder='Last Name'
+                        />
+                        <FormInput
+                            name="userName"
+                            label='Username'
+                            type='text'
+                            placeholder='Username'
+                        />
+                    </div>
+                    <SubmitButton size='lg' text="Create Profile" />
+                </FormContainer>
+            </div>
+        </section>
+    )
+}
+
+export default CreateProfile
