d23f1f75898e2eea1dbffd26d64ac0d06b599ccd
Sroysawan Kladcheydee <sroysawan.kcd@gmail.com>
2025-01-01 22:33:20 +0700

    feat: Enhance Search component with query parameter handling and debounced updates

--- components/Navbar/Search.tsx
+++ components/Navbar/Search.tsx
@@ -1,11 +1,44 @@
+'use client'
+import { useRouter, useSearchParams } from "next/navigation"
 import { Input } from "../ui/input"
-
+import { useEffect, useState } from "react"
+import { useDebouncedCallback } from 'use-debounce';
 const Search = () => {
+  const searchParams = useSearchParams()
+  const { replace } = useRouter()
+
+  const [search, setSearch] = useState(
+    searchParams.get('search')?.toString() || ''
+  )
+
+  const handleSearch = useDebouncedCallback((value:string)=>{
+    const params = new URLSearchParams(searchParams)
+    //ถ้ามี
+    if(value){
+      params.set('search', value)
+    } else {
+      //ถ้าไม่มี
+      params.delete('search')
+    }
+    replace(`/?${params.toString()}`)
+    
+  },500)
+
+  useEffect(()=>{
+    if(!searchParams.get('search')){
+      setSearch('')
+    }
+  },[searchParams])
   return (
     <Input
       type="text"
       placeholder="Search Camping"
       className="max-w-xs"
+      onChange={(e)=>{
+        setSearch(e.target.value)
+        handleSearch(e.target.value)
+      }}
+      value={search}
     />
   )
 }
