0ee1e32f5c80de9370146220d0d0d7c31f6a50be
Sroysawan Kladcheydee <sroysawan.kcd@gmail.com>
2025-01-02 16:56:20 +0700

    feat: add search and category filtering for landmarks

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,18 @@
+import LoadingCardHome from "@/components/card/LoadingCardHome"
+import LandmarkContainer from "@/components/home/LandmarkContainer"
 import { Button } from "@/components/ui/button"
+import { Suspense } from "react"
+const page = async({searchParams}:
+  {searchParams:{ search?:string ,category?:string}}) => {
+  //search
+  const {search,category} = await searchParams
 
-const page = () => {
   return (
-    <div>
-
-    </div>
+    <section>
+      <Suspense fallback={<LoadingCardHome/>}>
+        <LandmarkContainer search={search} category={category}/>
+      </Suspense>
+    </section>
   )
 }
 
