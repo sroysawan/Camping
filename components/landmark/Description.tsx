05cde7bb7edc13ad4af10b307c984923ce60b6f1
Sroysawan Kladcheydee <sroysawan.kcd@gmail.com>
2025-01-02 16:41:33 +0700

    feat: add Description component for displaying detailed text

---
+++ components/landmark/Description.tsx
@@ -0,0 +1,12 @@
+
+const Description = ({ description }: { description: string }) => {
+    return (
+        <article className="mt-4">
+            <p className="text-xl text-muted-foreground font-light leading-loose">
+                {description}
+            </p>
+        </article>
+    )
+}
+
+export default Description
